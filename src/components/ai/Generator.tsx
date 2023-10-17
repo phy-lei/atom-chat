588dfeba760cb01c00e29891b37e1458739a76ea
phy-lei <765373325@qq.com>
2023-10-17 11:22:25 +0800

    chore: 调试

--- src/components/ai/Generator.tsx
+++ src/components/ai/Generator.tsx
@@ -136,6 +136,12 @@ export default (props: { sessionImg: string }) => {
 
       while (!done) {
         const { value, done: readerDone } = await reader.read();
+        console.log(
+          '%c [ xxx ]',
+          'font-size:13px; background:pink; color:#bf2c9f;',
+          value,
+          done
+        );
         if (value) {
           const char = decoder.decode(value);
           if (char === '\n' && currentAssistantMessage().endsWith('\n'))
